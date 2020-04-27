961971bb3f78842ba044cbdeaff7f00f8a2b903f
lraimond <raimondilelio@gmail.com>
2020-04-27 23:30:25 +0200

    Add files via upload

--- .github/workflows/aggregator.py
+++ .github/workflows/aggregator.py
@@ -10,7 +10,7 @@ import pandas as pd
 
 files = ["templ1.xlsx","templ2.xlsx","templ3.xlsx"]
 
-# Read data model 
+# Read data model and produce list of sheets
 dm = pd.read_excel("datamodel.xlsx")
 sheets = list(dict.fromkeys(dm['Sheet']) )
 
@@ -21,15 +21,15 @@ obj = pd.DataFrame (columns=["File","Sheet","Row","Column","Value"])
 # Read all datapoints required in the data model and put them in the
 # object database
 for sheet in sheets:
+    subdm = dm[dm.Sheet == sheet]
     for file in files:
         cf = pd.read_excel(file, sheet_name=sheet, header=None) 
-        for i in range (0,dm.shape[0]):
-            Z = dm.iloc[i,0]
-            X = dm.iloc[i,1]
-            Y = dm.iloc[i,2]
-            if (Z==sheet):
-                D = cf.iloc[X-1,Y-1]
-                obj = obj.append({'File' : file , 'Sheet' : Z, 
+        for i in range (0,subdm.shape[0]):
+            Z = subdm.iloc[i,0]
+            X = subdm.iloc[i,1]
+            Y = subdm.iloc[i,2]
+            D = cf.iloc[X-1,Y-1]
+            obj = obj.append({'File' : file , 'Sheet' : Z, 
                       'Row' : X,'Column' : Y,'Value' : D,} , ignore_index=True)
         
 # Write the object database to a file
